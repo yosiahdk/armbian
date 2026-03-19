9eb6f022c75e7975636cae3764b0c223f36e9d85
yosiahdk <53029317+yosiahdk@users.noreply.github.com>
2026-03-19 14:18:09 +0700

    Update Box64.sh

--- Box64.sh
+++ Box64.sh
@@ -13,13 +13,13 @@ if [ -f /usr/share/keyrings/box64-archive-keyring.gpg ]; then
   sudo rm -f /usr/share/keyrings/box64-archive-keyring.gpg
 fi
 sudo mkdir -p /usr/share/keyrings
-wget -qO- "https://pi-apps-coders.github.io/box64-debs/KEY.gpg" | sudo gpg --dear>
+wget -qO- "https://pi-apps-coders.github.io/box64-debs/KEY.gpg" | sudo gpg --dearmor -o /usr/share/keyrings/box64-archive-keyring.gpg
 
 # create .sources file
 echo "Types: deb
 URIs: https://Pi-Apps-Coders.github.io/box64-debs/debian
 Suites: ./
-Signed-By: /usr/share/keyrings/box64-archive-keyring.gpg" | sudo tee /etc/apt/sou>
+Signed-By: /usr/share/keyrings/box64-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/box64.sources >/dev/null
 
 sudo apt update
 sudo apt install box64-generic-arm -y
